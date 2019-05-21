d90f0264b8372cca862ee32410c8e777c50bb64f
ChengqunYang <1521410544@qq.com>
2019-05-21 16:20:35 +0800

    PredicateFunctionalInterface

---
+++ basis_code/functionalInterface/src/demo05/Predicate/Demo04Predicate_negate.java
@@ -0,0 +1,35 @@
+package demo05.Predicate;
+
+import java.util.function.Predicate;
+
+/*
+    需求:
+        判断字符串的长度是否大于5,
+        如果长度大于5那么返回false,
+        如果不大于5,结果返回true
+        所以我们可以使用取反符号!,对判断的结果进行取反操作
+
+        Predicate接口中有一个默认方法negate,也表示取反的意思
+
+ */
+public class Demo04Predicate_negate {
+     /*
+        定义一个方法,方法的参数,传递一个字符串
+        传递一个Predicate接口
+            一个用于判断字符串的长度是否大于5
+     */
+     public static boolean checkString(String s, Predicate<String> pre) {
+         return !pre.test(s);
+     }
+
+    public static void main(String[] args) {
+        //定义一个字符串,
+        String s = "abd";
+        //调用checkString 方法,传递参数字符串和Lambda表达式
+        boolean b = checkString(s,(str)->{
+            //判断字符串的长度是否大于5,并返回结果
+            return str.length()>5;
+        });
+        System.out.println(b);
+    }
+}
