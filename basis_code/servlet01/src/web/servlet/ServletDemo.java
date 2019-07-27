9319bfcca54b84ae7769550a236bf4f73a6e844f
ChengqunYang <1521410544@qq.com>
2019-07-27 22:49:01 +0800

    Signed-off-by: ChengqunYang <1521410544@qq.com>

--- basis_code/servlet01/src/web/servlet/ServletDemo.java
+++ basis_code/servlet01/src/web/servlet/ServletDemo.java
@@ -6,6 +6,11 @@ import java.io.IOException;
 
 @WebServlet("/demo")
 public class ServletDemo implements Servlet{
+    /*
+        servlet的init方法只执行一次,说明一个servlet在内存中只存在一个对象,servlet是单例的
+            多个用户同时访问时可能会出现线程安全的问题
+            解决:尽量不要在servelt中定义成员变量,即使定义了成员变量也不要对其修改值
+     */
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
 
--- basis_code/tomcat01/src/web/servlet/ServletDemo1.java
+++ basis_code/tomcat01/src/web/servlet/ServletDemo1.java
@@ -5,6 +5,28 @@ import java.io.IOException;
 
 /**
  * Servlet的快速入门
+ * tomcat部署项目的方式:
+ *      1.直接将项目放到webapps目录下即可
+ *          1.1 /hello:项目的访问路径-->虚拟路径
+ *          1.2 简化部署:将项目打成一个war包,在京war包放置到webappps目录下.war包会自动解压缩
+ *      2.配置conf/server.xml文件
+ *          在<Host>标签体中配置
+ *          <Context docBase="D:\hello" path="/hehe" />
+ *              docbase:项目存放的路径
+ *              path:虚拟目录
+ *      3.在conf\catalina\localhost创建任意名称的xml文件,在文件中编写
+ *          <Context docBase="D:\hello" />
+ *              虚拟目录:xml文件的名称
+ *          热部署:修改完配置文件后并不需要重启服务器即可生效
+ * 静态项目和动态项目:
+ *  1. 目录结构:
+ *      1.1 java动态项目的目录结构:
+ *          -- 项目的根目录
+ *              -- WEB-INF目录
+ *                  --web.xml:web项目的核心配置文件
+ *                  --classes目录:放置字节码文件的目录
+ *                  --lib目录:放置依赖的jar包
+ *
  */
 public class ServletDemo1 implements Servlet{
     @Override
